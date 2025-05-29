53628ab3cc4a7e7f76af0a80d59cd4985a1ecf04
yukuri611 <yukiiw611@gmail.com>
2025-05-30 06:39:33 +0900

    ベルマン方程式に基づいた更新式によるブートストラップアルゴリズムでの更新

---
+++ ch04/dp.py
@@ -0,0 +1,15 @@
+V = {'L1': 0.0, 'L2': 0.0}
+new_V = V.copy()
+
+cnt = 0
+while True:
+    new_V['L1'] = 0.5 * (-1 + 0.9 * V['L1']) + 0.5 * (1 + 0.9 * V['L2'])
+    new_V['L2'] = 0.5 * (0 + 0.9 * V['L1']) + 0.5 * (-1 + 0.9 * V['L2'])
+
+    delta = max(abs(new_V['L1'] - V['L1']), abs(new_V['L2'] - V['L2']))
+    V = new_V.copy()
+    cnt += 1
+    if delta < 0.0001:
+        print(V)
+        print("Iterations:", cnt)
+        break
